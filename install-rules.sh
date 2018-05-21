b1f7db18d098aac69161a12b1115e0a944d6970f
brian <@laskow26>
2018-05-21 10:07:19 -0400

    new install setup

--- install-rules.sh
+++ install-rules.sh
@@ -1,3 +1,5 @@
+PWD=$(pwd)
+
 #initialize
 sleep 1
 echo "This tool will added LW yara rules to your maldect configuration to improve malware scans"
@@ -7,6 +9,8 @@ if [[ -x $(which maldet) ]] 2> /dev/null; then #maldet installed
 
 	#backup existing rules
 	cp -av /usr/local/maldetect/sigs/rfxn.yara{,.bak}
+	cp -av $PWD/lw-rules_index.yar /usr/local/maldetect/sigs/
+	cp -av $PWD/includes /usr/local/maldetect/sigs/includes
 	#add combined ruleset
 	sed -i s/"sigdir\/rfxn.yara"/"sigdir\/lw-rules_index.yar"/g /usr/local/maldetect/internals/internals.conf
 	#for i in $(cut -d '"' -f 2 lw-rules_index.yar); do cat $i >>  /usr/local/maldetect/sigs/rfxn.yara; done
